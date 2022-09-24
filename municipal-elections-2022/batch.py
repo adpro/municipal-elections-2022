4848a889edb2dbd54f77b7ff53431c29e3d389c0
Aleš Daniel <ales.daniel@adpro.cz>
2022-09-24 12:40:39 +0200

    Batch script update to 2022 elections
    Add parameter for minimum batch number
    Add exception handling

--- municipal-elections-2022/batch.py
+++ municipal-elections-2022/batch.py
@@ -1,4 +1,5 @@
 import requests
+import argparse
 from xml.etree import ElementTree
 
 
@@ -13,8 +14,8 @@ def download_batch(id: int):
         id = ""
 
     # SOAP request URL
-    date_str = "20181005"
-    url = "https://volby.cz/pls/kv2018/vysledky_okrsky?datumvoleb="+date_str+"&davka=" + str(id)
+    # url = "https://volby.cz/pls/kv2018/vysledky_okrsky?datumvoleb=20181005&davka=" + str(id)
+    url = "https://www.volby.cz/pls/kv2022/vysledky_okrsky?davka=" + str(id)
 
     # headers
     headers = {
@@ -37,15 +38,38 @@ def contains_org(org_id, xml):
 if __name__ == "__main__":
     org_id = '562394' # CK
     org_batches = []
-    xml_response = download_batch(0)
-    root = ElementTree.fromstring(xml_response)
-    el = root.find('.//base:DAVKA', ns)
-    batch_max = 0
-    if el is not None:
-        batch_max = int(el.attrib['PORADI_DAVKY'])
-    for i in range(1, batch_max+1):
-        print(f"Downloading batch id={i}...")
-        xml = download_batch(i)
-        if contains_org(org_id, xml):
-            org_batches.append(xml)
-    print(f"Amount of batches with org_id: {len(org_batches)}.")
+    batch_min = 1
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', "--min", help = "minimal batch id", type=int, default=1)
+    args = parser.parse_args()
+    if args.min:
+        batch_min = args.min
+    else:
+        print(f"Minimal batch number: {args.min} has wrong format. Using default one..")
+
+    try:
+        xml_response = download_batch(0)
+        root = ElementTree.fromstring(xml_response)
+        el = root.find('.//base:DAVKA', ns)
+        batch_max = 0
+        if el is not None:
+            batch_max = int(el.attrib['PORADI_DAVKY'])
+        if batch_min > batch_max:
+            batch_min = 1
+        for i in range(batch_min, batch_max+1):
+            print(f"Downloading batch id={i}...")
+            xml = download_batch(i)
+            if contains_org(org_id, xml):
+                org_batches.append(xml)
+        print(f"Amount of batches with org_id: {len(org_batches)}.")
+    except requests.exceptions.ConnectionError as e:
+        print("Error in downloading data!")
+        print(f"Type of error: {type(e).__name__}")
+        print(f"Reason: {e.args}")
+    except Exception as e:
+        print("Error in processing data!")
+        print(f"Type of error: {type(e).__name__}")
+        print(f"Reason: {e}")
+    except KeyboardInterrupt:
+        print("\nProgram aborted! Exiting...")
